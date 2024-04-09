ebaa979801998a35aa24c594e430c5a39e33838c
Ahlistar <nicolaslew005@gmail.com>
2024-04-09 10:12:09 -0300

    le puse jamon

--- index.js
+++ index.js
@@ -38,6 +38,14 @@ app.get('/hola', async (req, res) => {
     }
 })
 
+app.get('/jamon', async (req, res) => {
+    try{
+      res.status(200).send({"msg": "El jamon con queso es rico"})
+      }catch(e){
+        res.status(500).send({'error': 'Internal server error'})
+      }
+  })
+
 
 app.post('/jugador', async (req, res) => {
   try{
