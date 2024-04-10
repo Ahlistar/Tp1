e0b830e289b53f733006ff93be3e95858681630d
Ahlistar <nicolaslew005@gmail.com>
2024-04-10 17:10:49 -0300

    STILL DONT KNOW

--- index.js
+++ index.js
@@ -2,23 +2,18 @@ const express = require('express');
 const cors = require('cors');
 const path = require('path');
 const bodyParser = require('body-parser');
+const serveStatic = require('serve-static');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Middleware
-const path = require('path'); // Import the path module
-
-// ... (other code)
-
-app.use(express.static(__dirname));
-app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
+app.use(serveStatic(path.join(__dirname, 'public')));
 
 // Routes
 app.get('/jamon', async (req, res) => {
@@ -40,11 +35,11 @@ app.post('/recetas', async (req, res) => {
 
 // Error handling middleware
 app.use((err, req, res, next) => {
-    console.error(err.stack);
-    res.status(500).json({ error: 'Internal server error' });
+  console.error(err.stack);
+  res.status(500).json({ error: 'Internal server error' });
 });
 
 // Start the server
 app.listen(PORT, () => {
-    console.log(`La cocina esta en http://localhost:${PORT}`);
+  console.log(`La cocina esta en http://localhost:${PORT}`);
 });
