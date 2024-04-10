6b014a3294e0ac3d56bb10641cbdeffde6e00103
Ahlistar <nicolaslew005@gmail.com>
2024-04-10 17:04:52 -0300

    YA NO SE QUE HAGO ASHDHDASJWJWJJSHDAWJHDASJHKAWSHJDKAWJHDKAWSD

--- index.js
+++ index.js
@@ -7,12 +7,18 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Middleware
+const path = require('path'); // Import the path module
+
+// ... (other code)
+
+app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
+
 
 // Routes
 app.get('/jamon', async (req, res) => {
