bc3876dc1f6933e5a9ae868ac7230d79091002f4
Ahlistar <nicolaslew005@gmail.com>
2024-04-10 18:23:00 -0300

    ICON + CLEAN

--- index.js
+++ index.js
@@ -20,6 +20,28 @@ app.get('/jamon', async (req, res) => {
     res.status(200).send({"msg": "jamon con espinaca q rico"})
     }catch(e){
       res.status(500).send({'error': 'Internal server error'})
+const express = require('express')
+const cors = require('cors');
+const path = require('path'); // Import path module
+const bodyParser = require('body-parser');
+
+const app = express();
+const port = 3000;
+
+
+//COSAS QUE NO SE QUE HACEN :D
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use(cors());app.use(express.json());app.use(express.urlencoded({ extended: true }));app.use(express.static(path.join(__dirname, 'public')));
+
+
+// ENDPOINTS ACA ABAJO :3
+app.get('/jamon', async (req, res) => {
+  try{
+    res.status(200).send({"msg": "jamon con espinaca q rico"})
+    }catch(e){
+      res.status(500).send({'error': 'Internal server error'})
     }
 })
 
@@ -37,3 +59,5 @@ app.post('/recetas', async (req, res) => {
 app.listen(port, () => {
   console.log(`La cocina esta en http://localhost:${port}`);
 });
+    }
+})
