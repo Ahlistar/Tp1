933cc146f3329db2ffe51077c5f9d157a6879f5d
Ahlistar <nicolaslew005@gmail.com>
2024-04-10 19:48:38 -0300

    FINAL POSTA (OSEA SOLO CON LO ESTETICO ME REFIERO PORFAVOR NO ESTA TERMINADO ESTO)

--- index.js
+++ index.js
@@ -10,7 +10,6 @@ const port = 3000;
 //COSAS QUE NO SE QUE HACEN :D
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-
 app.use(cors());app.use(express.json());app.use(express.urlencoded({ extended: true }));app.use(express.static(path.join(__dirname, 'public')));
 
 // ENDPOINTS ACA ABAJO :3
